19c7617d293228a204b6aeaed925622b86bdbd3c
Leif <stu226812@mail.uni-kiel.de>
2022-12-08 14:05:07 +0100

    day02

--- src/day02/Day02.java
+++ src/day02/Day02.java
@@ -10,15 +10,25 @@ import java.util.stream.Collectors;
 public class Day02 {
 	public static void main(String[] args) throws IOException {
 
-		ArrayList<String> lines = new ArrayList<>(
-				Files.readAllLines(Paths.get(".\\src\\day02\\Input")));
-		List<Integer[]> converted = lines.stream().map(a -> new Integer[] {(a.charAt(0)-'A'+1),(a.charAt(2)-'X'+1)}).collect(Collectors.toList());
+		ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get(".\\src\\day02\\Input")));
+		List<Integer[]> converted = lines.stream()
+				.map(a -> new Integer[] { (a.charAt(0) - 'A' + 1), (a.charAt(2) - 'X' + 1) })
+				.collect(Collectors.toList());
 		System.out.println(duell(converted));
+		System.out.println(taskB(converted));
 	}
-	
+
 	private static int duell(List<Integer[]> list) {
-		return list.stream().map(a -> (a[0] == a[1]	? 3	: (a[0] == (a[1] + 1) % 3 + 1) ? 6 : 0) + a[1]).reduce(0, Integer::sum);
+		return list.stream().map(a -> (a[0] == a[1] ? 3 : (a[0] == (a[1] + 1) % 3 + 1) ? 6 : 0) + a[1]).reduce(0,
+				Integer::sum);
 	}
-	
-	private
+
+	private static int taskB(List<Integer[]> list) {
+		return list
+				.stream().map(
+						a -> (a[1] == 2 ? 3 + a[0]
+								: (a[1] == 1 ? (a[0] == 1 ? 3 : a[0] - 1)
+										: (a[1] == 3 ? 6 + (a[0] == 3 ? 1 : a[0] + 1) : null))))
+				.reduce(0, Integer::sum);
+	}
 }
